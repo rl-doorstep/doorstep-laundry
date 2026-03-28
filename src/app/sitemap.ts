4fd54f3f2477bc6ce40b0682beb952df779955f0
rl-doorstep <ricky@doorsteplaundrylc.com>
2026-03-28 08:00:56 -0600

    update sitemap generation code (#18)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -16,7 +16,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     { path: "", changeFrequency: "weekly", priority: 1 },
     { path: "login", changeFrequency: "monthly", priority: 0.8 },
     { path: "signup", changeFrequency: "monthly", priority: 0.8 },
-    { path: "book", changeFrequency: "weekly", priority: 0.9 },
+    { path: "pricing", changeFrequency: "weekly", priority: 0.9 },
     { path: "forgot-password", changeFrequency: "yearly", priority: 0.5 },
     { path: "welcome", changeFrequency: "monthly", priority: 0.6 },
     { path: "reset-password", changeFrequency: "yearly", priority: 0.4 },
